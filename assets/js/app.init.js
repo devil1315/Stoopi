ba686743fa8b43e86250f1e7b7457bd44c5e8811
devil1315 <rohit131511@authenticode.in>
2024-06-03 17:59:56 +0530

    first commit

---
+++ assets/js/app.init.js
@@ -0,0 +1,10 @@
+var userSettings = {
+  Layout: "vertical",
+  SidebarType: "full",
+  BoxedLayout: true,
+  Direction: "ltr",
+  Theme: "light",
+  ColorTheme: "Blue_Theme",
+  cardBorder: false,
+};
+
